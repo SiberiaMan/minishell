632d5727aeba25ca82ba2ffa98d66b7019c6e750
Brittaney Mysaria <bmysaria@ox-s1.msk.21-school.ru>
2021-05-07 22:31:26 +0300

    norme: builtins.h

--- Misha/builtins/builtins.h
+++ Misha/builtins/builtins.h
@@ -11,21 +11,21 @@ void	free_vars(char **vars, int i);
 int		count_vars(char **vars);
 char	**copy_envp(char **envp);
 char	**export_one_var(char *var, char **envp, t_line_n_mask *l_n_m,
-					  t_token *token);
+			t_token *token);
 char	**check_for_equal_sign(char **vars);
 int		export_validation(char **vars);
 char	**collect_valid_vars(int count, char **vars, t_line_n_mask *l_n_m,
-						  t_token *token);
+			t_token *token);
 char	**manage_duplication(char **vars, char **envp, t_line_n_mask *l_n_m,
-						  t_token *token);
-int	ft_echo(char **str);
-int ft_cd(t_token *token, t_line_n_mask *l_n_m);
-int ft_pwd();
-int ft_env(char **envp);
-char *return_new_pwd();
-int	ft_unset(t_line_n_mask *l_n_m, t_token *token);
-char **unset(char **vars, char **envp, t_line_n_mask *l_n_m, t_token *token);
-int ft_exit(t_line_n_mask *l_n_m, t_token *token);
+			t_token *token);
+int		ft_echo(char **str);
+int		ft_cd(t_token *token, t_line_n_mask *l_n_m);
+int		ft_pwd(void);
+int		ft_env(char **envp);
+char	*return_new_pwd(void);
+int		ft_unset(t_line_n_mask *l_n_m, t_token *token);
+char	**unset(char **vars, char **envp, t_line_n_mask *l_n_m, t_token *token);
+int		ft_exit(t_line_n_mask *l_n_m, t_token *token);
 void	sort_vars(char **v);
 void	free_export(t_line_n_mask *l_n_m, t_token *token, char **vars, int i);
 int		ft_export(t_line_n_mask *l_n_m, t_token *token);
